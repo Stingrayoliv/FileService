34e12661e5ed86e6153cdc40a930bae1f5d82baa
Stingrayoliv <64040033+Stingrayoliv@users.noreply.github.com>
2020-11-25 18:37:10 +0100

    A file config service, which returns a path was implemented

---
+++ src/main/java/de/telran/processor/service/FileConfigService.java
@@ -0,0 +1,31 @@
+package de.telran.processor.service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class FileConfigService {
+    private Properties prop = new Properties();
+
+    public FileConfigService() throws Exception {
+        loadProperties();
+    }
+
+    public String getPathToSavedImages(String path_to_saves_images) {
+        return prop.getProperty("path_to_saves_images");
+    }
+
+    private void loadProperties() {
+        try {
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            InputStream stream = loader.getResourceAsStream("application.properties");
+            if (stream == null) {
+                System.out.println("Sorry, unable to find application.properties");
+                return;
+            }
+            prop.load(stream);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
