13e24c9ff9f40604fd542a9305a5e5ff31ddd88d
Stingrayoliv <64040033+Stingrayoliv@users.noreply.github.com>
2020-12-08 13:37:53 +0100

    FileService was extended for supporting image saving

---
+++ src/test/java/de/telran/processor/service/FileServiceTest.java
@@ -0,0 +1,14 @@
+package de.telran.processor.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileServiceTest {
+    @Test
+    public void getPathToSavedImages() throws Exception {
+        FileConfigService service = new FileConfigService();
+        String answer = service.getPathToSavedImages("path_to_saves_images");
+        assertEquals("~/Downloads/images/", answer);
+    }
+}
